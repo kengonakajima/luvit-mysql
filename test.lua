2a31d422aa678febeb6efd40f002b7e2aba4cc2a
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-02-11 12:51:18 +0900

    output image

---
+++ test.lua
@@ -0,0 +1,44 @@
+local timer = require( "timer" ) -- luvit built-in
+local mysql = require( "luvit-mysql" )
+
+local client = mysql.createClient( { db="",user="",port="",pass="" } )
+
+client:connect()
+
+client:query( "CREATE DATABASE testdb", function(err) 
+    if err and err ~= mysql.ERROR_DB_CREATE_EXISTS then
+      error("cannot create db" )
+    end
+  end)
+
+
+client:query( "USE testdb" )
+
+client:query( "DROP TABLE IF EXISTS testtable" )
+
+client:query( "CREATE TABLE testtable (id INT(11) AUTO_INCREMENT, name VARCHAR(255), age INT(11), created DATETIME, PRIMARY KEY (id) )",
+  function(err)
+    assert( not err )
+
+    client.query( "INSERT INTO testtable SET name = 'george', age = 20, created=now()", function(err) assert( not err ) end)
+    client.query( "INSERT INTO testtable SET name = 'jack', age = 30, created=now()", function(err) assert( not err ) end)    
+    client.query( "INSERT INTO testtable SET name = 'ken', age = 40, created=now()", function(err) assert( not err ) end)
+    
+  end)
+
+
+-- use timer because we're not sure about above INSERT finishes before following SELECT.
+timer.setInterval( 2000, function()
+    print("timer interval!")
+    
+    local q = client:query( "SELECT * FROM testtable", function(err) assert(not err) end )
+
+    q:on("row", function(row)
+        print("got a row:", row.name, row.age, row.created )
+      end)
+
+    q:on("end", function()
+        print("query finished" )
+      end)
+
+  end)
