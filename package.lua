8abb7f0d944dc1b496cdbb34a31e0c9d4cc99015
BRY402 <105960244+BRY402@users.noreply.github.com>
2024-02-12 17:08:12 -0300

    Added package support to fork

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "luvit-mysql",
+    version = "0.0.1",
+    description = "Luvit port of node-mysql .",
+    tags = { "lua", "lit", "luvit", "node", "mysql", "port", "db", "database" },
+    license = "Apache",
+    author = { name = "kengonakajima", email = "none" },
+    homepage = "https://github.com/kengonakajima/luvit-mysql",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
